688396e6a4f182fdca8bc60613469d1d3f203584
Gabin Ishimwe <87947641+Gabin-ishimwe@users.noreply.github.com>
2022-12-03 17:56:47 +0000

    update be link

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import {
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "https://graphql-flashcard-api.herokuapp.com/graphql",
+  uri: "https://flashcard-api-production.up.railway.app/graphql",
 });
 
 interface Token {
